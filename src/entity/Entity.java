764509ae8577251685a50cae3f0f7d936df11f19
Nhatnguyen557 <nguyenvietnhat557@gmail.com>
2026-04-22 20:34:25 +0700

    src

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,12 +1,7 @@
 package entity;
 
-import java.awt.Graphics;
 
-public abstract class Entity {
-    protected int x,y,speed;
-    protected int direction;
-
-    public abstract void update();
-    public abstract void draw(Graphics g);
-
-}
+public interface Entity {
+//TODO  
+} 
+ 
