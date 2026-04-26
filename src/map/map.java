9a504e270a9bc19340a0e45c53a767765481e8e4
DioBrta <90105852+DioBrta@users.noreply.github.com>
2026-04-26 09:34:23 +0700

    src

--- src/map/map.java
+++ src/map/map.java
@@ -1,72 +1,99 @@
-public class Map {
-    public static final int WALL = 1;
-    public static final int EMPTY = 0;
-    public static final int FOOD = 2;
-    public static final int POWER = 3;
+import java.util.ArrayList;
+import java.util.List;
 
-    private int[][] grid;
+public class Map {
+
+    private int rows;
+    private int cols;
+    private int tileSize = 32;
+
+    private char[][] grid;
+
+    private int pacmanRow, pacmanCol;
+    private List<int[]> ghostSpawns = new ArrayList<>();
+
+    private String[] tileMap = {
+        "XXXXXXXXXXXXXXXXXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X                 X",
+        "X XX X XXXXX X XX X",
+        "X    X       X    X",
+        "XXXX XXXX XXXX XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXrXX X XXXX",
+        "O       bpo       O",
+        "XXXX X XXXXX X XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXXXX X XXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X  X     P     X  X",
+        "XX X X XXXXX X X XX",
+        "X    X   X   X    X",
+        "X XXXXXX X XXXXXX X",
+        "X                 X",
+        "XXXXXXXXXXXXXXXXXXX"
+    };
 
     public Map() {
-        loadLevel();
+        loadMap();
     }
 
-    private void loadLevel() {
-        grid = new int[][] {
-            {1,1,1,1,1,1},
-            {1,0,2,0,3,1},
-            {1,0,1,0,0,1},
-            {1,2,0,2,0,1},
-            {1,1,1,1,1,1}
-        };
-    }
+    private void loadMap() {
+        rows = tileMap.length;
+        cols = tileMap[0].length();
 
-    public int getCell(int row, int col) {
-        return grid[row][col];
-    }
+        grid = new char[rows][cols];
 
-    public void setCell(int row, int col, int value) {
-        grid[row][col] = value;
-    }
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
 
-    public boolean isWall(int row, int col) {
-        return grid[row][col] == WALL;
-    }
+                char tile = tileMap[r].charAt(c);
+                grid[r][c] = tile;
 
-    public boolean canMove(int row, int col) {
- 
-        if (row < 0 || row >= grid.length ||
-            col < 0 || col >= grid[0].length) {
-            return false;
-        }
+                // detect Pac-Man spawn
+                if (tile == 'P') {
+                    pacmanRow = r;
+                    pacmanCol = c;
+                }
 
-        return grid[row][col] != WALL;
-    }
-
-    public boolean hasFood(int row, int col) {
-        return grid[row][col] == FOOD;
-    }
-
-    public boolean hasPower(int row, int col) {
-        return grid[row][col] == POWER;
-    }
-    public void eat(int row, int col) {
-        if (grid[row][col] == FOOD || grid[row][col] == POWER) {
-            grid[row][col] = EMPTY;
-        }
-    }
-
-    public boolean isAllFoodCleared() {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == FOOD || grid[i][j] == POWER) {
-                    return false;
+                // detect ghosts
+                if (tile == 'r' || tile == 'b' || tile == 'p' || tile == 'o') {
+                    ghostSpawns.add(new int[]{r, c});
                 }
             }
         }
-        return true;
     }
 
-    public int[][] getGrid() {
-        return grid;
+    public boolean canMove(int row, int col) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) {
+            return false;
+        }
+
+        return grid[row][col] != 'X';
     }
-}
+
+    public boolean eatFood(int row, int col) {
+        if (grid[row][col] == ' ') {
+            grid[row][col] = '.'; // mark as eaten
+            return true;
+        }
+        return false;
+    }
+ 
+    public char getTile(int row, int col) {
+        return grid[row][col];
+    }
+
+    public int getPacmanRow() { return pacmanRow; }
+    public int getPacmanCol() { return pacmanCol; }
+
+    public List<int[]> getGhostSpawns() {
+        return ghostSpawns;
+    }
+
+    public int getRows() { return rows; }
+    public int getCols() { return cols; }
+
+    public int getTileSize() { return tileSize; }
